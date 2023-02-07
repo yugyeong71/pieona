898a8e63ae44730c650ed86f7bd21cc6d66c4fe8
yugyeong <youkyoung71@naver.com>
2023-02-07 17:22:43 +0900

    init

---
+++ src/main/java/com/example/pieona/PieonaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pieona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PieonaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pieona/PieonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pieona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
