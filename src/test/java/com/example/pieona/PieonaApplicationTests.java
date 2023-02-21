a9a1fee04f24354b9cb9cec869fc38e9f8dc4c63
yugyeong <youkyoung71@naver.com>
2023-02-21 23:58:38 +0900

    Fix : @SpringBootTest 주석 처리

--- src/test/java/com/example/pieona/PieonaApplicationTests.java
+++ src/test/java/com/example/pieona/PieonaApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.pieona;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PieonaApplicationTests {
 
     @Test
